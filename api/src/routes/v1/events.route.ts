f1f354558508cae561a551d47e39dd3d0fde4690
Thomas Holmes <thomasholmes@outlook.com>
2024-02-04 16:40:37 +0000

    debug output.

--- api/src/routes/v1/events.route.ts
+++ api/src/routes/v1/events.route.ts
@@ -29,7 +29,7 @@ router.get('/', async (req: Request, res: Response) => {
       })
     }));
 
-    res.status(200).json(req.headers);
+    res.status(200).json(headerArray);
   } catch (error) {
     console.error('An error ocurred:', error);
     res.status(500).json(error);
