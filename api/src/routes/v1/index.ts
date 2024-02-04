07c87c5cb7546c8601c2d863b3b779abc0c34e2b
Thomas Holmes <thomasholmes@outlook.com>
2024-02-04 15:18:40 +0000

    Added event route.

--- api/src/routes/v1/index.ts
+++ api/src/routes/v1/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import books from './books.route';
+import events from './events.route';
 
 const router = Router();
 
 router.use('/books', books);
+
+router.use('/events', events);
 
 export default router;
 
